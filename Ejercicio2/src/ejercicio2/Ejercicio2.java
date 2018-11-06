7dbc6b19569ac182f50a85b3fa9c15615acbc332
ldizbarros <ldizbarros@perlanegra13.danielcastelao.org>
2018-11-06 10:35:46 +0100

    Ejercicio 2 Terminado

---
+++ Ejercicio2/src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,25 @@
+package ejercicio2;
+
+/**
+ * Programa que crea 4 fíos, os executa e espera a que estes terminen. 
+ * Os fíos escriben 5 veces o número de iteración do bucle e o seu nome. 
+ * En cada iteración, despois de escribir o seu nome, bloquéanse durante un 
+ * segundo e volven a estar dispoñibles para a súa execución.
+ * @author laura
+ */
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        
+        Fio1 f1 = new Fio1();
+        Fio2 f2 = new Fio2();
+        Fio3 f3 = new Fio3();
+        Fio4 f4 = new Fio4();
+        
+        f1.start();
+        f2.start();
+        f3.start();
+        f4.start();
+    }
+    
+}
---
+++ Ejercicio2/src/ejercicio2/Fio2.java
@@ -0,0 +1,20 @@
+package ejercicio2;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Fio2 extends Thread{
+    
+    @Override
+    public void run(){
+        for (int i=1;i<=5;i++){
+            System.out.println("Fio2 -- "+i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Fio1.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+}
