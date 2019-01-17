868fc75f55937b60393c0c5ebf65b0e6bb1b52f6
ldizbarros <ldizbarros@perlanegra13.danielcastelao.org>
2019-01-17 09:58:12 +0100

    Ejercicio Sockets - Servidor enviar/recibir 3 mensajes

--- SocketServerTCP/src/socketservertcp/SocketServerTCP.java
+++ SocketServerTCP/src/socketservertcp/SocketServerTCP.java
@@ -2,6 +2,7 @@ package socketservertcp;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -14,6 +15,7 @@ public class SocketServerTCP {
 
     public static void main(String[] args) throws IOException {
         
+        boolean reciviendo = false;
         //Creamos el socket del servidor
         System.out.println("Creando socket servidor");
 	ServerSocket serverSocket=new ServerSocket();
@@ -27,20 +29,33 @@ public class SocketServerTCP {
         //En cuenato reciba una conexion se crea el objeto Socket
 	System.out.println("Aceptando conexiones");
         Socket newSocket= serverSocket.accept();
-
-        //Se crea un stream que recibira los datos que envie el cliente
-        System.out.println("Conexión recibida");
-        InputStream input = newSocket.getInputStream();
-        //OutoutStream os=newSocket.getOutputStream(); esto solo se necesita si se envian mensajes
-
-        //Creamos una variable qu enos permitira visializar el mensaje.
-        //Grabamos en esa variable lo que nos llega en el input
-	byte[] mensaje=new byte[25];
-	input.read(mensaje);
-
-        //Mostramos el mensaje
-	System.out.println("Mensaje recibido: "+new String(mensaje));
-
+        reciviendo = true;
+        int count = 0;
+        
+        while (reciviendo==true){
+            count++;
+            //Se crea un stream que recibira los datos que envie el cliente
+            System.out.println("Conexión recibida");
+            InputStream input = newSocket.getInputStream();
+            OutputStream output=newSocket.getOutputStream();
+            
+            if (count>3){
+                reciviendo=false;
+            }else{
+                String mensaje="Mensaje numero "+count;
+                output.write(mensaje.getBytes());
+                System.out.println("Mensaje "+count+" enviado");
+            
+                //Creamos una variable qu enos permitira visializar el mensaje.
+                //Grabamos en esa variable lo que nos llega en el input
+                byte[] mensajeRe=new byte[20];
+                int leer = input.read(mensajeRe);
+                
+                //Mostramos el mensaje
+                System.out.println("Mensaje recibido: "+new String(mensaje));
+            }
+        }
+        
         //Se cierra el socket que lee 
 	System.out.println("Cerrando el nuevo socket");
 	newSocket.close();
