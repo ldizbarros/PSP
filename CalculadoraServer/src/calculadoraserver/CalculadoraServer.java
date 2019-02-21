40a93ac2b6e49ed91392901fa3eb38a98865bdbc
ldizbarros <ldizbarros@perlanegra13.danielcastelao.org>
2019-02-21 10:17:19 +0100

    Caculadora Server v1 Terminado

--- CalculadoraServer/src/calculadoraserver/CalculadoraServer.java
+++ CalculadoraServer/src/calculadoraserver/CalculadoraServer.java
@@ -36,76 +36,87 @@ public class CalculadoraServer {
         System.out.println("Realizando el bind");   
         InetSocketAddress addr=new InetSocketAddress("localhost",6666);
 	serverSocket.bind(addr);
-
+        
         //El socket del servidor se queda escuchando en la direccion deseada.
         //En cuenato reciba una conexion se crea el objeto Socket
 	System.out.println("Aceptando conexiones");
         Socket newSocket= serverSocket.accept();
-           
-        //Se crea un stream que recibira los datos que envie el cliente
-        InputStream input = newSocket.getInputStream();
-        System.out.println("Conexión recibida");
-            
-        byte[] mensajeRe=new byte[20];
-        int leer = input.read(mensajeRe);
-                
-        //Mostramos el mensaje
-        System.out.println("Cadena recibida: "+new String(mensajeRe));
 
-        double resultado = 0,num1 = 0,num2 = 0;
-        int operacion = 0;
-        String cadenaRecibida = new String(mensajeRe);
-        String [] cadena =cadenaRecibida.split(";");
+        String cadenaRecibida="";
         
-        try{
-            operacion = Integer.valueOf(cadena[0]);
-            num1 = Integer.valueOf(cadena[1]);
-            num2 = Integer.valueOf(cadena[2]);
-        }catch(NumberFormatException e2){
-            e2.getMessage();   
-        }
-        
-        String mensaje="";
-        
-        try{   
-            switch (operacion) {
-                case 1:
-                    resultado = num1 + num2;
-                    mensaje=String.valueOf(resultado);
-                    break;
-                case 2:
-                    resultado = num1 - num2;
-                    mensaje=String.valueOf(resultado);
-                    break;
-                case 3:
-                    resultado = num1 * num2;
-                    mensaje=String.valueOf(resultado);
-                    break;
-                case 4:
-                    resultado= num1 / num2;
-                    mensaje=String.valueOf(resultado);
-                    break;
-                case 5:
-                    if (num1>0){
-                        resultado = (int) Math.sqrt(num1);
-                    }else{
-                        mensaje="Error!";
-                    }
-                    break;
-                default:
-                    System.out.println("Se ha producido un error en la operacion recibida");
-                    mensaje="Se ha producido un error en la operacion recibida";
-            }
-        }catch(ArithmeticException ex){
-            System.out.println("La operacion introducida no es posible realizarla");
-            mensaje="La operacion introducida no es posible realizarla";
-        }
-        
-        System.out.println("El resultado es: "+String.valueOf(resultado));
+        while(true){
+            //Se crea un stream que recibira los datos que envie el cliente
+            InputStream input = newSocket.getInputStream();
+            System.out.println("Conexión recibida");
             
-        OutputStream output=newSocket.getOutputStream();
-        output.write(mensaje.getBytes());
-        System.out.println("Mensaje enviado");
+            byte[] mensajeRe=new byte[20];
+            int leer = input.read(mensajeRe);
+                
+            cadenaRecibida = new String(mensajeRe);
+
+            if (cadenaRecibida.contains("0")){
+                break;
+            }else{
+                //Mostramos el mensaje
+                System.out.println("Cadena recibida: "+cadenaRecibida);
+
+                double resultado = 0,num1 = 0,num2 = 0;
+                int operacion = 0;
+
+                String [] cadena =cadenaRecibida.split(";");
+
+                try{
+                    operacion = Integer.valueOf(cadena[0]);
+                    num1 = Integer.valueOf(cadena[1]);
+                    num2 = Integer.valueOf(cadena[2]);
+                }catch(NumberFormatException e2){
+                    e2.getMessage();   
+                }
+
+                String mensaje="";
+
+                try{   
+                    switch (operacion) {
+                        case 1:
+                            resultado = num1 + num2;
+                            mensaje=String.valueOf(resultado);
+                            break;
+                        case 2:
+                            resultado = num1 - num2;
+                            mensaje=String.valueOf(resultado);
+                            break;
+                        case 3:
+                            resultado = num1 * num2;
+                            mensaje=String.valueOf(resultado);
+                            break;
+                        case 4:
+                            resultado= num1 / num2;
+                            mensaje=String.valueOf(resultado);
+                            break;
+                        case 5:
+                            if (num1>0){
+                                resultado = (double) Math.sqrt(num1);
+                                mensaje=String.valueOf(resultado);
+                            }else{
+                                mensaje="Error!";
+                            }
+                            break;
+                        default:
+                            System.out.println("Se ha producido un error en la operacion recibida");
+                            mensaje="Se ha producido un error en la operacion recibida";
+                    }
+                }catch(ArithmeticException ex){
+                    System.out.println("La operacion introducida no es posible realizarla");
+                    mensaje="La operacion introducida no es posible realizarla";
+                }
+
+                System.out.println("El resultado es: "+String.valueOf(resultado));
+
+                OutputStream output=newSocket.getOutputStream();
+                output.write(mensaje.getBytes());
+                System.out.println("Mensaje enviado");
+            }
+        }
 
         //Se cierra el socket que lee 
 	System.out.println("Cerrando el nuevo socket");
