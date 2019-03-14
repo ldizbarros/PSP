abb9883e8f80284429be43e9b3cb921713572998
ldizbarros <ldizbarros@perlanegra13.danielcastelao.org>
2019-03-14 14:09:40 +0100

    Chat Server Basico Funcionano

---
+++ Chat_Server/src/chat_server/ChatServer.java
@@ -0,0 +1,91 @@
+package chat_server;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Práctica programación fíos: desenvolvemento de un chat con arquitectura cliente/servidor.
+ * O obxectivo é desenvolver un sistema de chat cliente/servidor mediante sockets en TCP/IP.
+ * O servidor:
+ * 1. Permitirá conectar varios clientes de chat simultanemente, ata un máximo de 10. X
+ * 2. Disporá dunha única sala de chat á que se conectarán todos os clientes. X
+ * 3. Cada usuario terá un nickname que se lle solicitará antes de establecer  
+ * a conexión co servidor. V
+ * 4. Amosará por pantalla todas as mensaxes que se reciban desde os clientes 
+ * a medida que van chegando, indicando "nickname: mensaxe..." V
+ * 5. Cada mensaxe que se reciba será reenviado a todos os clientes, incluíndo 
+ * o nickname correspondente. X
+ * 6. Ao arrancar o servidor, se solicitará o porto polo que se establecerá a conexión. V
+ * 7. Cada vez que se conecte un novo cliente, indicarase por pantalla:
+ *      Novo cliente conectado (nickname / enderezo IP / porto ) V
+ *      Actualmente hai x usuarios conectados.
+ * 8. Mentras non se conecte ningún usuario, ou se todos os clientes se desconectan, 
+ * amosarase a mensaxe "Ningún cliente conectado". X
+ * 9.Se o servidor é pechado, todos os clientes cerrarán adecuadamente as súas
+ * conexións tras amosar a mensaxe "O servidor desconectouse". X
+ * @author ldizbarros
+ */
+public class ChatServer {
+    
+    static ArrayList <Cliente> listaClientes = new ArrayList();
+
+    public static void main(String[] args){
+        //Pedimos el puerto del servidor.
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduzca el PUERTO del servidor --> ");
+        int puerto = sc.nextInt();
+        
+        try {
+            //Creamos el socket del servidor
+            ServerSocket serverSocket=new ServerSocket();
+            InetSocketAddress addr=new InetSocketAddress("localhost",puerto);
+            serverSocket.bind(addr);
+            System.out.println("Socket Servidor creado");
+            
+            InputStream input;
+            OutputStream output;
+            
+            while(true){
+                if (listaClientes.size() <10) {
+                    //El socket del servidor se queda escuchando en la direccion deseada.
+                    //En cuenato reciba una conexion se crea el objeto Socket
+                    System.out.println("Aceptando conexiones");
+                    Socket newSocket= serverSocket.accept();
+
+                    input = newSocket.getInputStream();
+                    output = newSocket.getOutputStream();
+
+                    //Recibimos el nickname
+                    byte[] nicknameRE = new byte[250];
+                    input.read(nicknameRE);
+                    String nickname = new String(nicknameRE);
+                    System.out.println("Usuario solicita conexion:"+nickname);
+                    //Se crea un hilo por cada conexion al server
+                    
+                    //Añadimos un nuevo cliente a la lista y lo iniciamos
+                    Cliente nuevoCliente = new Cliente(newSocket,nickname,output,input);
+                    nuevoCliente.start();
+                    listaClientes.add(nuevoCliente);
+                    
+                    System.out.println(nickname+" se ha conectado (IP:"+newSocket.getLocalAddress()
+                            +", PUERTO: "+newSocket.getLocalPort()+")");
+                    System.out.println("Hay " + listaClientes.size() + " cliente/s conectados.");
+                    
+                    for (Cliente elemento: listaClientes) {
+                        String conexionNueva = nickname+" se ha conectado al chat.\n"; 
+                        elemento.enviarMensaje(conexionNueva);
+                    }
+                }
+                
+            }
+        } catch (IOException ex) {
+            System.out.println("Se ha producido un error en la conexion.");
+        }
+    }
+}
---
+++ Chat_Server/src/chat_server/Cliente.java
@@ -0,0 +1,71 @@
+package chat_server;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Clase donde se define el hilo.
+ * @author ldizbarros
+ */
+public class Cliente extends Thread{
+    
+    private Socket socket;
+    private InputStream input;
+    private OutputStream output;
+    private String nickname;
+    
+    public Cliente(Socket socket,String nickname,OutputStream output,InputStream input) throws IOException{
+        this.socket=socket; 
+        this.nickname=nickname;
+        this.input = input;
+        this.output = output;
+    }
+    
+    @Override
+    public void run(){
+        try{
+            while (true) {
+                byte[] mensajeRe=new byte[250];
+                //Se crea un stream que recibira los mensajes
+                input.read(mensajeRe);
+                //Mostramos el mensaje
+                String mensajeRecibido = new String(mensajeRe);
+                System.out.println("Se ha recibido el mesaje: "+mensajeRecibido);
+                if (mensajeRecibido.contains("/bye")){
+                    ChatServer.listaClientes.remove(this);
+                    System.out.println("Hay "+ChatServer.listaClientes.size()+" usuarios conectados");
+                    
+                    for (Cliente elemento: ChatServer.listaClientes) {
+                        String conexion = nickname+" se ha desconectado.\n"; 
+                        elemento.enviarMensaje(conexion);
+                    }
+                    //Cerramos la conexión
+                    socket.close();
+                    System.out.println(nickname+" se ha desconectado");
+                    break;
+                }else{
+                    for (Cliente elemento: ChatServer.listaClientes) {
+                        String mensaje = nickname+": "+ mensajeRecibido;   
+                        elemento.enviarMensaje(mensaje);
+                    }
+                    System.out.println("Mensaje enviado");
+                }
+            }
+        }catch(IOException ex){
+            System.out.println("Se ha producido un error en la conexion");
+        }
+    }
+    
+    public void enviarMensaje(String mensaje){
+        try {
+            output.write(mensaje.getBytes());
+        } catch (IOException ex) {
+            System.out.println("Se ha producido un error al enviar el mensaje");
+        }
+        System.out.println("Mensaje enviado");
+    }
+}
